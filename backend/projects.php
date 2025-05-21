932ffb048590675eefd0427106d5ab662707cb7b
HauntFox172 <bugorki68ruru@gmail.com>
2025-05-21 17:01:53 +0300

    Add projects.php

--- backend/projects.php
+++ backend/projects.php
@@ -1,5 +1,25 @@
-<!-- 
-    TODO: 
-    Get => projects; 
-    Post (data) { json = data }; 
--->
+<?php
+header('Content-Type: application/json');
+$file = '../data/projects.json';
+
+// GET
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo file_get_contents($file);
+}
+
+// SET
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $projects = json_decode(file_get_contents($file), true);
+    
+    $newProject = [
+        'id' => uniqid(),
+        'title' => htmlspecialchars($data['title']),
+        'description' => htmlspecialchars($data['description']),
+        'likes' => 0
+    ];
+    
+    $projects[] = $newProject;
+    file_put_contents($file, json_encode($projects));
+    echo json_encode($newProject);
+}
